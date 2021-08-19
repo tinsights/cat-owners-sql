74fe2ec3d929c7baea6645b86dca3a1daba5d8c9
Tinaes <aj.tinaes@gmail.com>
2021-08-19 11:50:57 +0800

    removed queries from innerjoin.sql

--- init_tables.sql
+++ init_tables.sql
@@ -1,6 +1,2 @@
 CREATE TABLE IF NOT EXISTS owners (id SERIAL PRIMARY KEY, name TEXT);
 CREATE TABLE IF NOT EXISTS cats (id SERIAL PRIMARY KEY, name TEXT, owner_id INTEGER);
-
-SELECT owner_id from cats GROUP BY owner_id HAVING COUNT(owner_id) > 1;
-
-SELECT owner_id FROM cats WHERE name='Muji';
